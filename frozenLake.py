342c006865903307b70d19d83296f374960e84f7
kpta119 <kacper.pt119@gmail.com>
2025-01-10 18:34:36 +0100

    dodanie metod nagradzających

--- frozenLake.py
+++ frozenLake.py
@@ -1,12 +1,24 @@
 import gymnasium as gym
 from matplotlib import pyplot as plt
 import numpy as np
+import random
+
+MAP = '''SFFFFFFF
+FFFFFFFF
+FFFHFFFF
+FFFFFHFF
+FFFHFFFF
+FHHFFFHF
+FHFFHFHF
+FFFHFFFG'''
+
+MAP = [ state for state in MAP if state in ['S', 'F', 'H', 'G']]
 
 
 class FrozenAgent:
     def __init__(self, state_size: int,
                 action_size: int,
-                learning_rate :float = 0.05,
+                learning_rate :float = 0.1,
                 discount_factor :float = 0.95,
                 exploration_epsilon :float = 1.0):
         self.state_size = state_size
@@ -17,7 +29,7 @@ class FrozenAgent:
         self.qtable = np.zeros((state_size, action_size))
 
     def choose_action(self, state: int) -> int:
-        if np.random.random() < self.exploration_epsilon: # z prawdopodobieństwem epsilon wybieramy akcje losową
+        if random.uniform(0,1) < self.exploration_epsilon: # z prawdopodobieństwem epsilon wybieramy akcje losową
             return np.random.randint(self.action_size)
         return np.argmax(self.qtable[state, :])
 
@@ -27,10 +39,26 @@ class FrozenAgent:
         self.qtable[state, action] += self.learning_rate * delta
 
     def update_epsilon(self) -> None:
-        self.exploration_epsilon = max(0.1, self.exploration_epsilon * 0.9995)
+        self.exploration_epsilon = max(0.01, self.exploration_epsilon * 0.99)
+
+def reward_default(next_state):
+    if next_state == 63:
+        return 1
+    else:
+        return 0
 
 
-def Qlearing(env, agent: FrozenAgent, max_steps=200, num_episodes=1000):
+def reward_hole_minus(next_state):
+    reward = 0
+    if MAP[next_state] == 'H':
+        reward = -2
+    elif MAP[next_state] == 'G':
+        reward = 10
+    return reward
+
+
+
+def Qlearing(env, agent: FrozenAgent, max_steps=200, num_episodes=1000, rewarding=reward_default):
     rewards = np.zeros(num_episodes)
     e = 0
     while e < num_episodes:
@@ -41,9 +69,12 @@ def Qlearing(env, agent: FrozenAgent, max_steps=200, num_episodes=1000):
             action = agent.choose_action(state)
             next_state, reward, done, _, _ = env.step(action)
 
+            reward = rewarding(next_state)
             agent.update_qtable(state, action, reward, next_state)
+
             total_reward += reward
             state = next_state
+            #env.render()
 
             if done:
                 break
@@ -56,20 +87,18 @@ def Qlearing(env, agent: FrozenAgent, max_steps=200, num_episodes=1000):
     return rewards
 
 
-def count_averaged_reward(env, agent, max_steps=200, num_episodes=1000,  num_of_ind_runs=25):
+def count_averaged_reward(env, max_steps=200, num_episodes=1000,  num_of_ind_runs=15):
     averaged_reward = np.zeros(num_episodes)
     for i in range(num_of_ind_runs):
-        averaged_reward += Qlearing(env, agent, max_steps, num_episodes)
+        agent = FrozenAgent(env.observation_space.n, env.action_space.n)
+        averaged_reward += Qlearing(env, agent, max_steps, num_episodes, reward_hole_minus)
     return averaged_reward / num_of_ind_runs
 
 
 def main():
     env = gym.make('FrozenLake-v1', desc=None, map_name="8x8", is_slippery=False)
-    agent = FrozenAgent(env.observation_space.n, env.action_space.n, learning_rate=0.8, discount_factor=0.95, exploration_epsilon=1.0)
-    #averaged_reward_base = count_averaged_reward(env, agent)
-    rewards = Qlearing(env, agent)
-    print(rewards)
-
+    averaged_reward_base = count_averaged_reward(env)
+    print(averaged_reward_base)
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
     ax.spines['left'].set_position('center')
